bbd3bb04d5dc89526658c661ca71af32e7476281
Masoud Heydari <m.heydari4883@gmail.com>
2021-12-31 18:28:57 +0330

    add images

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from model.FaceDetection import FaceDetection
 
-
 if __name__ == '__main__':
-    face_detection = FaceDetection(detect_eye=False )
+    face_detection = FaceDetection(detect_eye=False)
     face_detection.start_face_detection()
